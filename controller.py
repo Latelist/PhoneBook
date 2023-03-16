82b9d7f3b431c425d2f07f3f91e5f946d3863356
Salatchen <pozhivi.s.moe@gmail.com>
2023-03-16 23:34:36 +0600

    new function: delete. Code made clearer

--- controller.py
+++ controller.py
@@ -4,23 +4,28 @@ lst = [1,2,3,4,5,6]
 
 def control():
     while True:
+        path = 'file.txt'
         req = request()
         if req == 1:
             contact = save_data()
-            write_data(contact)
+            write_data(path, contact)
         elif req == 2:
             string = output()
-            print(read_data(string))
+            print(read_string(path, string))
         elif req == 3:
             string = output()
-            contacts = change_data(string)
-            new_data(contacts)
+            contacts = change_data(path, string)
+            new_data(path, contacts)
         elif req == 4:
             p = parameter()
             sort(p)
         elif req == 5:
             print_data()
         elif req == 6:
+            string = output()
+            contacts = delete_data(path, string)
+            new_data(path, contacts)
+        elif req == 7:
             break
         else:
             req = int(input('Повторите ввод: '))
--- database.py
+++ database.py
@@ -1,48 +1,63 @@
-def write_data(contact):
-    with open('file.txt', 'a', encoding = 'utf-8') as data:
+def new_data(path, contacts):
+    with open(path, 'w') as data:
+        for i in contacts:
+            data.write(f'{i}\n')
+
+def write_data(path, contact):
+    with open(path, 'a') as data:
         for i in contact:
             data.write(f'{i} ')
 
-def read_data(string):
-    result = 0
-    with open('file.txt', 'r') as data:
+def read_data(path):
+    with open(path, 'r') as data:
         contacts = data.readlines()
-        for i in contacts:
-            if string in i:
-                result = i
+    contacts = [i.strip() for i in contacts]
+    return contacts
+
+def read_string(path, string):
+    contacts = read_data(path)
+    for i in contacts:
+        if string in i:
+            result = i
     return result
 
-def change_data(string):
-    with open('file.txt', 'r+') as data:
-        contacts = data.readlines()
-        contacts = [i.strip() for i in contacts]
-        for i in range(len(contacts)):
-            if string in contacts[i]:
-                print(contacts[i])
+def change_data(path, string):
+    contacts = read_data(path)
+    for i in range(len(contacts)):
+        if string in contacts[i]:
+            print(contacts[i])
+            r = int(input('Изменить этот контакт? Введите 0, чтобы удалить. Введите 1, чтобы продолжить поиск: '))
+            if r == 0:
                 print('Измените контакт')
                 surname = input('Введите фамилию: ')
                 name = input('Введите имя: ')
                 number = input('Введите телефон: ')
                 contacts[i] = f'{surname} {name} {number}'
+                break
+        elif i == len(contacts) -1:
+            print('Извините, в книге нет такого контакта.')
+            print()
     return contacts
 
-def new_data(contacts):
+def sort(path, p):
+    contacts = read_data(path)
+    contacts = sorted(contacts, key = lambda x: x.split()[p])
     with open('file.txt', 'w') as data:
         for i in contacts:
+            print(i)
             data.write(f'{i}\n')
 
-def sort(p):
-    with open('file.txt', 'r+') as data:
-        contacts = data.readlines()
-        contacts = [i.strip() for i in contacts]
-        contacts = sorted(contacts, key = lambda x: x.split()[p])
-        with open('file.txt', 'w') as data:
-            for i in contacts:
-                print(i)
-                data.write(f'{i}\n')
-
-def print_data():
-    with open('file.txt', 'r') as data:
-        contacts = data.readlines()
-    for i in contacts:
-        print(i)
+def delete_data(path, string):
+    contacts = read_data(path)
+    for i in range(len(contacts)):
+        if string in contacts[i]:
+            print(contacts[i])
+            r = int(input('Удалить этот контакт? Введите 0, чтобы удалить. Введите 1, чтобы продолжить поиск: '))
+            if r == 0:
+                contacts.remove(contacts[i])
+                break
+            print()
+        elif i == len(contacts) -1:
+            print('Извините, в книге нет такого контакта.')
+            print()
+    return contacts
